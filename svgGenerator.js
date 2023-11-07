258073d6cb88cb1af5351debbeee0d05c30bf88b
Wolffkran <liebherr2008@live.com>
2023-11-07 08:35:26 -0500

    Finally fixed the conditionals to generate shapes

--- svgGenerator.js
+++ svgGenerator.js
@@ -1,14 +1,24 @@
 // Export your SVG generation function
 function createSVG(text, textColor, shape, shapeColor) {
-    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      <rect width="300" height="200" fill="${shapeColor}" />
-      <text x="150" y="100" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
-    </svg>`;
-  
-    return svgContent;
+  let shapeContent = '';
+
+  // Conditionally generate the shape based on the user's input
+  if (shape === 'circle') {
+    shapeContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+  } else if (shape === 'triangle') {
+    shapeContent = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
+  } else if (shape === 'square') {
+    shapeContent = `<rect x="50" y="50" width="200" height="200" fill="${shapeColor}" />`;
   }
-  
-  module.exports = {
-    createSVG,
-  };
-  
+
+  const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+    ${shapeContent}
+    <text x="150" y="100" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
+  </svg>`;
+
+  return svgContent;
+}
+
+module.exports = {
+  createSVG,
+};
